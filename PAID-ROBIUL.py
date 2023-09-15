a1a004443321e65c8a962d1bdeb7135411bd87f7
Robiul-Facker <144569883+Robiul-Facker@users.noreply.github.com>
2023-09-15 08:57:46 +0600

    PAID-ROBIUL.py

--- PAID-ROBIUL.py
+++ PAID-ROBIUL.py
@@ -519,7 +519,6 @@ def api1(ids,names,passlist):
                                 'fb_api_req_friendly_name':'authenticate',
                         }
                         headers={
-                                'headers = {
                                 'authority': 'www.facebook.com',
                                 'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7',
                                 'accept-language': 'en-US,en;q=0.9',
